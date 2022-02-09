d6ba57112b1ba9825376b23ade6a1f957e5cfaae
Ivan Ropovik <ivan.ropovik@gmail.com>
2022-02-09 10:19:31 +0100

    bayesian design analysis

--- bayesianFactorAnalysis.R
+++ bayesianFactorAnalysis.R
@@ -1,11 +1,5 @@
----
-  title: "Simulation for the multi-site"
-output:
-  html_document: default
-pdf_document: default
----
+#Bayesian design analysis settings
 
-# Bayesian design analysis settings
 nLabsExpected <- 42 # The number of labs that committed to data collection
 labMinN <- 70 # Minimum required N per lab
 labMaxN <- 130 # Stopping N for labs
@@ -14,21 +8,25 @@ effectSize <- rnorm(1e6, .2, .05) # Distribution of plausible, yet conservativel
 
 # Libraries
 library(BFDA)
+library(parallel)
+# readRDS("simResults.RDS")
 
-minN <- (nLabs*labMinN/nConditions)
-maxN <- (nLabs*labMaxN/nConditions)
+minN <- (nLabsExpected*labMinN/nConditions)
+maxN <- (nLabsExpected*labMaxN/nConditions)
 
 simH1 <- BFDA.sim(expected.ES = effectSize, type = "t.between", design = "sequential",
                    prior = list("Cauchy", list(prior.location = 0, prior.scale = sqrt(2)/2)),
                    n.min = minN, n.max = maxN, alternative = "two.sided", boundary = 10, B = 1000,
-                   verbose = TRUE, cores = 4, stepsize = 10)
+                   verbose = TRUE, cores = detectCores(), stepsize = 10)
 
 simH0 <- BFDA.sim(expected.ES = 0, type = "t.between", design = "sequential",
                    prior = list("Cauchy", list(prior.location = 0, prior.scale = sqrt(2)/2)),
                    n.min = minN, n.max = maxN, alternative = "two.sided", boundary = 10, B = 1000,
-                   verbose = TRUE, cores = 4, stepsize = 10)
+                   verbose = TRUE, cores = detectCores(), stepsize = 10)
 
 (simResults <- list(
+  simObjectH1 = simH1,
+  simObjectH0 = simH0,
   resultsH1 = BFDA.analyze(simH1, design = "sequential", boundary = 10),
   resultsH0 = BFDA.analyze(simH0, design = "sequential", boundary = 10)))
 
@@ -36,3 +34,5 @@ plot(simH1, boundary=c(1/10, 10))
 plotH1 <- recordPlot()
 plot(simH0, boundary=c(1/10, 10))
 plotH0 <- recordPlot()
+
+saveRDS(simResults, "simResults.RDS")
