dabba2785a6627d6fe1e0781d66efbd47ca5bc57
Ivan Ropovik <ivan.ropovik@gmail.com>
2022-02-11 14:09:20 +0100

    made knitr::spin friendly

--- bayesianFactorAnalysis.R
+++ bayesianFactorAnalysis.R
@@ -1,11 +1,41 @@
-#Bayesian design analysis settings
+#' ---
+#' title: "Bayesian design analysis"
+#' date: "`r Sys.Date()`"
+#' output:
+#'    html_document:
+#'       toc: true
+#'       toc_float: true
+#'       code_folding: show
+#'       fig_retina: 2
+#' always_allow_html: yes
+#' ---
 
-nLabsExpected <- 30 # Hedged number of labs. The number of labs that committed to data collection was 41
-labMinN <- 70 # Minimum required N per lab
-labMaxN <- 130 # Stopping N for labs
-nConditions <- 5 # Number of experimental conditions
-effectSize <- rnorm(1e6, .2, .05) # Distribution of plausible, yet conservatively small effect sizes, centered at .2 with 95% CI between .1 and .3
+#'# Simulation settings
+# Number of simulations
+nSim <- 70
+# Bayes factor boundary
+bfBoundary <- 10
 
+# Prior distribution
+priorDistribution <- "Cauchy"
+# Distribution location
+priorLocation <- 0
+# Distribution scale
+priorScale <- sqrt(2)/2
+
+# Hedged number of labs
+nLabsExpected <- 30 # The number of labs that committed to data collection was 41.
+# Minimum required N per lab
+labMinN <- 70
+# Stopping N for labs
+labMaxN <- 130 
+# Number of experimental conditions
+nConditions <- 5
+# Distribution of plausible, yet conservatively small effect sizes
+effectSize <- rnorm(1e6, .2, .05) # Centered at .2 with 95% CI between .1 and .3
+
+#+ setup, include = FALSE
+knitr::opts_chunk$set(echo=FALSE, warning = FALSE)
 # Libraries
 library(BFDA)
 library(parallel)
@@ -15,23 +45,35 @@ minN <- (nLabsExpected*labMinN/nConditions)
 maxN <- (nLabsExpected*labMaxN/nConditions)
 
 simH1 <- BFDA.sim(expected.ES = effectSize, type = "t.between", design = "sequential",
-                   prior = list("Cauchy", list(prior.location = 0, prior.scale = sqrt(2)/2)),
-                   n.min = minN, n.max = maxN, alternative = "two.sided", boundary = 10, B = 1000,
+                   prior = list(priorDistribution, list(prior.location = priorLocation, prior.scale = priorScale)),
+                   n.min = minN, n.max = maxN, alternative = "two.sided", boundary = bfBoundary, B = nSim,
                    verbose = TRUE, cores = detectCores(), stepsize = 10)
 
 simH0 <- BFDA.sim(expected.ES = 0, type = "t.between", design = "sequential",
-                   prior = list("Cauchy", list(prior.location = 0, prior.scale = sqrt(2)/2)),
-                   n.min = minN, n.max = maxN, alternative = "two.sided", boundary = 10, B = 1000,
+                   prior = list(priorDistribution, list(prior.location = priorLocation, prior.scale = priorScale)),
+                   n.min = minN, n.max = maxN, alternative = "two.sided", boundary = bfBoundary, B = nSim,
                    verbose = TRUE, cores = detectCores(), stepsize = 10)
 
-(simResults <- list(
+simResults <- list(
   simObjectH1 = simH1,
   simObjectH0 = simH0,
   resultsH1 = BFDA.analyze(simH1, design = "sequential", boundary = 10),
-  resultsH0 = BFDA.analyze(simH0, design = "sequential", boundary = 10)))
+  resultsH0 = BFDA.analyze(simH0, design = "sequential", boundary = 10))
 
+#'# Bayesian design analysis results
+
+#+ include = TRUE
+#'## H1 results
+simH1
+simResults$resultsH1
+#'### Sequential design analysis plot for H1
 plot(simH1, boundary=c(1/10, 10))
 plotH1 <- recordPlot()
+
+#'## H0 results
+simH0
+simResults$resultsH0
+#'### Sequential design analysis plot for H0
 plot(simH0, boundary=c(1/10, 10))
 plotH0 <- recordPlot()
 
